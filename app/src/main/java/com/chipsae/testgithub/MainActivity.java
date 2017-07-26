a2fb7544b098787ff8629684eb1bc03a40e0dafb
linjp <linjp@chipsea.com>
2017-07-26 17:45:38 +0800

    add featrue-1 function

--- app/src/main/java/com/chipsae/testgithub/MainActivity.java
+++ app/src/main/java/com/chipsae/testgithub/MainActivity.java
@@ -1,13 +1,30 @@
 package com.chipsae.testgithub;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Button btn_feature_1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    /**
+     * feature-1
+     * display HelloWorld
+     * @param view
+     */
+    public void feature1(View view) {
+        if (btn_feature_1.getText().toString().equals("功能1")) {
+            btn_feature_1.setText("功能1");
+        } else {
+            btn_feature_1.setText("功能1");
+        }
     }
 }
